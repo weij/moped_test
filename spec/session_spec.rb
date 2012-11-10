72df2142cd3d3379fbb42a8934b48e385e0aa9ad
Weijun Yin <gw9812@gmail.com>
2012-11-10 15:46:46 +0800

    Test session#command, #use, #new, #with

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -61,5 +61,74 @@ describe "Moped::Session" do
       end
     end
   end
+
+  describe "#command" do
+    it "should run the command on the current database" do
+      session.with(database: "moped_test_2") do |session|
+        session.command(dbStats: 1)["db"].should eq "moped_test_2"
+      end
+    end
+  end
+
+  describe "#new" do
+    it "should return a thread-safe session" do
+      session.command ping: 1
+
+      5.times.map do
+        Thread.new do
+          session.new do |new_session|
+            new_session.command ping: 1
+          end
+        end
+      end.each(&:join)
+    end
+  end
+
+  describe "#use" do
+    
+    after do
+      session.use "moped_test"
+    end
+
+    it "should change the current database" do
+      session.use "moped_test_2"
+      session.command(dbStats: 1)["db"].should eq "moped_test_2"
+    end
+  end
+
+  describe "#with" do
+
+    context "when called with a block" do
+      
+      it "returns the value from the block" do
+          session.with{:value}.should eq :value
+      end
+
+      it "yields a session with the provided options" do
+          session.with(safe: true) do |safe|
+            safe.options[:safe].should eq true
+          end
+      end  
+
+      it "does not modify the original session" do
+        session.with(database: "other") do |safe|
+          session.options[:database].should eq "moped_test"
+        end
+      end
+    end
+
+    context "when called without a block" do
+      
+      it "returns a session with the provided options" do
+        safe = session.with(safe: true)
+        safe.options[:safe].should eq true
+      end
+
+      it "does not modify the original session" do
+        session.with(database: "other")
+        session.options[:database].should eq "moped_test"
+      end
+    end
+  end
 end
 
